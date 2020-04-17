7eef984842091e8d0d92e8417f5566908f2feca7
Miguel Ramírez <miguelramidev@gmail.com>
2020-04-16 21:55:53 -0400

    Se creó el header del portafolio

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() selected: number = 0;
+
+  navOptions: {option: string, key: number}[] = [
+    {option: 'Inicio', key: 0},
+    {option: 'Sobre mí', key: 1},
+    {option: 'Habilidades', key: 2},
+    {option: 'Proyectos', key: 3},
+    {option: 'Contactame', key: 4},
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
