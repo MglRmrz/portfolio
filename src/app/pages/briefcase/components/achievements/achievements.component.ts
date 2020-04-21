bec682efb267eb60d4a25df3ceae121a581b31db
Miguel Ramírez <miguelramidev@gmail.com>
2020-04-20 23:16:29 -0400

    Se creó las sección de proyectos

--- src/app/pages/briefcase/components/achievements/achievements.component.ts
+++ src/app/pages/briefcase/components/achievements/achievements.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { MProject } from '@core/models/project.model';
 
 @Component({
@@ -9,10 +9,27 @@ import { MProject } from '@core/models/project.model';
 export class AchievementsComponent implements OnInit {
 
   @Input() projects: MProject[];
+  @ViewChild('divProjects', {static: true}) divProjects: ElementRef;
+  scrollPosition: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.divProjects);
+  }
+
+  /**
+   * @description Avanza al siguente proyecto
+   */
+  nextProject(): void {
+    this.divProjects.nativeElement.scrollTo({left: (this.divProjects.nativeElement.scrollLeft + 250), behavior: 'smooth'});
+  }
+
+  /**
+   * @description Retrocede al anterior proyecto
+   */
+  previousProject(): void {
+    this.divProjects.nativeElement.scrollTo({left: (this.divProjects.nativeElement.scrollLeft - 250), behavior: 'smooth'});
   }
 
 }
